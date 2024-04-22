f9880e72bc57202cdf10eb049df93f9926ca23a9
Luan-art <luanlf2003@gmail.com>
2024-04-22 18:56:19 -0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿int max = 0, numeroA = 1, numeroB = 1, aux = 0;
+
+do
+{
+    Console.WriteLine("Digite uma posicão para chegarmos na sequência FIbonacci");
+    max = int.Parse(Console.ReadLine());
+
+} while (max <= 0);
+
+Console.WriteLine("Fibonacci até a posição " + max);
+
+for (int i = 0; i < max; i++)
+{
+    Console.Write(numeroA + " ");
+
+    aux = numeroA;
+    numeroA = numeroB;
+    numeroB = aux + numeroB;
+}
